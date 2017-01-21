c16fffb98ca26287078efe9a8025eae67e7940cc
Tetsuwo OISHI <tetsuwo.oishi@gmail.com>
2017-01-22 00:08:27 +0900

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -4,5 +4,24 @@
 
 set -ue
 
-VERSION=$(python ./src/bin/memcached-stat.py --item=version)
-echo "${VERSION}"
+items=( \
+    "bytes"
+    "bytes_read" \
+    "bytes_written" \
+    "cmd_get" \
+    "cmd_set" \
+    "curr_connections" \
+    "curr_items" \
+    "evictions" \
+    "get_hits" \
+    "get_misses" \
+    "limit_maxbytes" \
+    "uptime" \
+    "version" \
+)
+
+for item in ${items[@]}
+do
+  value=$(python ./src/bin/memcached-stat.py --item=${item})
+  echo "${item}: ${value}"
+done
