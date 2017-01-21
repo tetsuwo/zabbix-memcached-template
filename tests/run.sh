fb179f36b505f715c8fb41569c0e99748bdb202c
Tetsuwo OISHI <tetsuwo.oishi@gmail.com>
2017-01-22 01:00:36 +0900

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -4,10 +4,20 @@
 
 set -ue
 
-items="bytes bytes_read bytes_written cmd_get cmd_set curr_connections curr_items evictions get_hits get_misses limit_maxbytes uptime version"
+digit_items="bytes bytes_read bytes_written cmd_get cmd_set curr_connections curr_items evictions get_hits get_misses limit_maxbytes uptime version"
 
-for item in $items
+for item in $digit_items
 do
   value=$(python ./src/bin/memcached-stat.py --item=${item})
   echo "${item}: ${value}"
+  expr "${value}" + 1 > /dev/null 2>&1
+  if [ $? -lt 2 ]; then
+    echo "Valid value"
+  else
+    echo "Invalid value"
+    exit 1
+  fi
 done
+
+
+string_items=""
