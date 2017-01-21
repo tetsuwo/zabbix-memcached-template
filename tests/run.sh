bab987bec2bea5029febadd914c875cc5bb48986
Tetsuwo OISHI <tetsuwo.oishi@gmail.com>
2017-01-22 01:06:16 +0900

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -4,20 +4,47 @@
 
 set -ue
 
-digit_items="bytes bytes_read bytes_written cmd_get cmd_set curr_connections curr_items evictions get_hits get_misses limit_maxbytes uptime version"
+digit_items="bytes bytes_read bytes_written cmd_get cmd_set curr_connections curr_items evictions get_hits get_misses limit_maxbytes uptime"
 
 for item in $digit_items
 do
   value=$(python ./src/bin/memcached-stat.py --item=${item})
-  echo "${item}: ${value}"
   expr "${value}" + 1 > /dev/null 2>&1
   if [ $? -lt 2 ]; then
-    echo "Valid value"
+    echo "Valid: ${item}=${value}"
   else
-    echo "Invalid value"
+    echo "Invalid: ${item}=${value}"
+    exit 1
+  fi
+  
+  value=$(python ./src/bin/memcached-stat.py -i ${item})
+  expr "${value}" + 1 > /dev/null 2>&1
+  if [ $? -lt 2 ]; then
+    echo "Valid: ${item}=${value}"
+  else
+    echo "Invalid: ${item}=${value}"
     exit 1
   fi
 done
 
 
-string_items=""
+string_items="version"
+
+for item in $string_items
+do
+  value=$(python ./src/bin/memcached-stat.py --item=${item})
+  if [ -n "${value} ]; then
+    echo "Valid: ${item}=${value}"
+  else
+    echo "Invalid: ${item}=${value}"
+    exit 1
+  fi
+  
+  value=$(python ./src/bin/memcached-stat.py -i ${item})
+  if [ -n "${value} ]; then
+    echo "Valid: ${item}=${value}"
+  else
+    echo "Invalid: ${item}=${value}"
+    exit 1
+  fi
+done
